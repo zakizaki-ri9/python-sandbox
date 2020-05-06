5ed15dd35f89f32543d882ea6d5eae9f7e46254c
zakizaki-ri9 <kic.yuuki@gmail.com>
2020-05-06 14:55:53 +0900

    fix: black lint

--- django-basic/config/urls.py
+++ django-basic/config/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+
+# パスコンバータ
 from django.urls import path
 
 urlpatterns = [
