d2187c4db46f6a0405ee6c7bf32667da4ff8aae5
zakizaki-ri9 <kic.yuuki@gmail.com>
2020-05-06 16:55:47 +0900

    feat: polls model and db migration

--- django-basic/polls/models.py
+++ django-basic/polls/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    """設問モデル"""
+
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+
+class Choice(models.Model):
+    """投票モデル"""
+
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
