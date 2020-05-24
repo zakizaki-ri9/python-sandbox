6b252ff19faf020ae4a270c82d9cd4e8f36534de
zakizaki-ri9 <kic.yuuki@gmail.com>
2020-05-24 10:37:50 +0900

    black formatted

--- django-rest-tutorial/snippets/migrations/0002_auto_20200523_1141.py
+++ django-rest-tutorial/snippets/migrations/0002_auto_20200523_1141.py
@@ -9,18 +9,23 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('snippets', '0001_initial'),
+        ("snippets", "0001_initial"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='snippet',
-            name='highlighted',
-            field=models.TextField(default=''),
+            model_name="snippet",
+            name="highlighted",
+            field=models.TextField(default=""),
         ),
         migrations.AddField(
-            model_name='snippet',
-            name='owner',
-            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='snippets', to=settings.AUTH_USER_MODEL),
+            model_name="snippet",
+            name="owner",
+            field=models.ForeignKey(
+                null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                related_name="snippets",
+                to=settings.AUTH_USER_MODEL,
+            ),
         ),
     ]
