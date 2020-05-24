0b0fbab33479899a3db339844c6828eab02d8f75
zakizaki-ri9 <kic.yuuki@gmail.com>
2020-05-24 10:55:44 +0900

    feat: user view

--- django-rest-tutorial/snippets/views.py
+++ django-rest-tutorial/snippets/views.py
@@ -5,6 +5,18 @@ from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework import mixins
 from rest_framework import generics
+from django.contrib.auth.models import User
+from snippets.serializers import UserSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
 class SnippetList(
