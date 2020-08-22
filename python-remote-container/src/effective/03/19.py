0896103f2a68697f2a75e0c2da19b015033b90c8
zakizaki-ri9 <kic.yuuki@gmail.com>
2020-08-22 16:25:25 +0900

    feat: effective python 19

---
+++ python-remote-container/src/effective/03/19.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+def get_stats(numbers: List[int]) -> (int, int, int, int, int):
+    minimum = min(numbers)
+    maximum = max(numbers)
+    return minimum, maximum, len(numbers), numbers[0], numbers[-1]
+
+
+lengths = [1, 3, 5, 9, 2, 8, 6]
+# minimum, maximum, first, last, count = get_stats(lengths) のように順番を間違えやすい
+minimum, maximum, count, first, last = get_stats(lengths)
+print(f"Min: {minimum}, Max: {maximum}, Count: {count}, first: {first}, last: {last}")
