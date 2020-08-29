b299d4b5df4933f6e2bf263d3f4c9c71074d6e30
zakizaki-ri9 <kic.yuuki@gmail.com>
2020-08-29 19:29:20 +0900

    feat: effective python 31 - best

--- python-remote-container/src/effective/04/31.py
+++ python-remote-container/src/effective/04/31.py
@@ -57,6 +57,34 @@ class Better:
                 yield float(line)
 
 
+class ReadVisits:
+    def __init__(self, data_path: str):
+        self.data_path = data_path
+
+    # 呼び出される or 終了するまで、iteratorが生成 or 回される
+    def __iter__(self):
+        with open(self.data_path) as f:
+            for line in f:
+                yield float(line)
+
+
+class Best:
+    @classmethod
+    def get_percentages(cls, data_path: str) -> List[float]:
+        visits = ReadVisits(data_path=data_path)
+        percentages = cls.__normalize(visits)
+        return percentages
+
+    @classmethod
+    def __normalize(cls, read_visits: ReadVisits) -> List[float]:
+        total = sum(read_visits)
+        result = []
+        for value in read_visits:
+            percent = 100 * value / total
+            result.append(percent)
+        return result
+
+
 data_path = os.path.join(os.getcwd(), __file__.replace(".py", ".txt"))
 
 # output: Anti=[] となる
@@ -64,3 +92,6 @@ print(f"Anti={Anti.get_percentages(data_path=data_path)}")
 
 # output: Better=[...] となる
 print(f"Better={Better.get_percentages(data_path=data_path)}")
+
+# output: Better=[...] となる
+print(f"Best={Best.get_percentages(data_path=data_path)}")
